5de395f99366d21d249cd8480126eab1c8b4cfd4
JorisPaarde <jorispaarde@gmail.com>
2025-02-08 22:18:36 +0100

    fixes markdown links

--- src/utils/remarkBasePath.mjs
+++ src/utils/remarkBasePath.mjs
@@ -4,7 +4,7 @@ export function remarkBasePath() {
   return function (tree) {
     const base = process.env.NODE_ENV === 'development' ? '' : '/webdev-inzichten';
 
-    visit(tree, ['image', 'html'], (node) => {
+    visit(tree, ['image', 'html', 'link'], (node) => {
       // Handle markdown images
       if (node.type === 'image' && node.url) {
         if (node.url.startsWith('/')) {
@@ -12,10 +12,24 @@ export function remarkBasePath() {
         }
       }
       
-      // Handle HTML img tags in markdown
+      // Handle markdown links
+      if (node.type === 'link' && node.url) {
+        if (node.url.startsWith('/')) {
+          node.url = `${base}${node.url}`;
+        }
+      }
+
+      // Handle HTML img tags and links in markdown
       if (node.type === 'html' && typeof node.value === 'string') {
+        // Handle img tags
         node.value = node.value.replace(
           /(<img[^>]*src=["'])(\/[^"']*)(["'][^>]*>)/g,
+          `$1${base}$2$3`
+        );
+        
+        // Handle anchor tags
+        node.value = node.value.replace(
+          /(<a[^>]*href=["'])(\/[^"']*)(["'][^>]*>)/g,
           `$1${base}$2$3`
         );
       }
