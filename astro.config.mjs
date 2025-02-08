f19827d4db3e6f61dbd3b4bca60e4f09ea4d0e5f
JorisPaarde <jorispaarde@gmail.com>
2025-02-08 21:47:30 +0100

    sets dynamic base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+const isDevelopment = process.env.NODE_ENV === 'development'
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://jorispaarde.github.io/webdev-inzichten/',
-	base: '/webdev-inzichten',
+	base: isDevelopment ? '' : '/webdev-inzichten',
 	integrations: [mdx(), sitemap()],
 });
