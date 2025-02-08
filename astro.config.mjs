ecc68ca046cc48342d5e696752af2f2cf45ba245
JorisPaarde <jorispaarde@gmail.com>
2025-02-08 21:12:43 +0100

    Add GitHub Actions workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://jorispaarde.github.io/webdev-inzichten/',
 	integrations: [mdx(), sitemap()],
 });
